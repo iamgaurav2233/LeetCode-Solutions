d540bd7dc0eea82c4ca5b870366269a4db588315
Gaurav dube <90771453+iamgaurav2233@users.noreply.github.com>
2023-12-02 01:55:26 +0530

    Update 2830. Maximize the Profit as the Salesman.cpp

--- Dynamic Programming/2830. Maximize the Profit as the Salesman.cpp
+++ Dynamic Programming/2830. Maximize the Profit as the Salesman.cpp
@@ -18,3 +18,23 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+public:
+    int maximizeTheProfit(int n, vector<vector<int>> &arr) {
+        n = arr.size();
+        sort(arr.begin(), arr.end());
+        vector<int> fi;
+        for (int i = 0;i < arr.size(); i++)
+            fi.push_back(arr[i][0]);
+        vector<int> dp(n + 1, 0);
+        for (int i = (int)arr.size() - 1;i >= 0; i--) {
+            dp[i] = max(dp[i + 1], arr[i][2]);
+            int it = upper_bound(fi.begin(), fi.end(), arr[i][1]) - fi.begin();
+            if (it != arr.size())
+                dp[i] = max(dp[i], dp[it] + arr[i][2]);
+        }      
+        return dp[0];
+    }
+};
