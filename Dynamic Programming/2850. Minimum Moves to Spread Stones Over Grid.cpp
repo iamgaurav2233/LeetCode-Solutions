26114665460bcff61182ff878d5bdae35dda367e
Gaurav dube <90771453+iamgaurav2233@users.noreply.github.com>
2023-12-07 21:00:59 +0530

    Update 2850. Minimum Moves to Spread Stones Over Grid.cpp

--- Dynamic Programming/2850. Minimum Moves to Spread Stones Over Grid.cpp
+++ Dynamic Programming/2850. Minimum Moves to Spread Stones Over Grid.cpp
@@ -1,32 +1,38 @@
 class Solution {
 public:
-      int cost(pair<int, int> &A, pair<int, int> &B) {
+    int cost(pair<int, int> &A, pair<int, int> &B) {
         return abs(A.first - B.first) + abs(A.second - B.second);
     }
+
     int minimumMoves(vector<vector<int>>& grid) {
         vector<pair<int, int>> A, B;
-        for (int i = 0;i < 3; i++) {
-            for (int j = 0;j < 3; j++) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 if (grid[i][j] > 1)
-                    for (int k = 0;k < grid[i][j] - 1; k++)
+                    for (int k = 0; k < grid[i][j] - 1; k++)
                         A.push_back({i, j});
                 if (grid[i][j] == 0)
                     B.push_back({i, j});
             }
         }
+
         int n = A.size();
-        vector<vector<int>> dp(n + 1, vector<int> ((1 << n), 1e6));
-        dp[0][0] = 0;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0;j < (1 << n); j++) {
-                if(__builtin_popcountll(j) != i - 1) continue;
-                int ans = 1e6;
-                for (int k = 0;k < n; k++) {
-                    if (!(j & (1 << k)))
-                        dp[i][j | (1 << k)] = min(dp[i][j | (1 << k)], dp[i - 1][j] + cost(A[i - 1], B[k]));
+        int m = (1 << n);
+        vector<vector<int>> dp(n + 1, vector<int>(m, 1e6));
+        
+        dp[n][m - 1] = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int mask = 0; mask < m; mask++) {
+                for (int j = 0; j < n; j++) {
+                    if (!(mask & (1 << j))) {
+                        int newMask = mask | (1 << j);
+                        dp[i][mask] = min(dp[i][mask], dp[i + 1][newMask] + cost(A[i], B[j]));
+                    }
                 }
             }
         }
-        return dp[n][(1 << n) - 1];
+
+        return dp[0][0];
     }
 };
